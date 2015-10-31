eb0a61ff5f7effdfcfaad82c7978bb2ec4c52a1c
Bruno Cascio <brunocascio@gmail.com>
2015-10-31 14:54:55 -0300

    Test commit for travisCI

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
 
-// require('./common/QueueTest')();
-// require('./common/BehaviorTest')();
-// require('./common/RequirementTest')();
-// require('./common/PageTest')();
-// require('./common/MemoryTest')();
-
+//require('./common/QueueTest')();
+//require('./common/BehaviorTest')();
+//require('./common/RequirementTest')();
+//require('./common/PageTest')();
+//require('./common/MemoryTest')();
 require('./algorithms/LruTest')();
 require('./algorithms/FifoTest')();
 require('./algorithms/OptimalTest')();
